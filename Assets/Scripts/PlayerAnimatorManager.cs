b9f26aeae7d0846a966a1a3d78bb3834b7a4a7f0
Suyatna <zuyatna@gmail.com>
2018-12-03 17:30:55 +0700

    adding *My Robot Kyle* and PlayerManager

--- Assets/Scripts/PlayerAnimatorManager.cs
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class PlayerAnimatorManager : MonoBehaviour {
 
+	private Animator playerAnimator;
+	
+	[SerializeField]
+	private float directionDampTime = 0.25f;
+
 	// Use this for initialization
 	void Start () {
+		
+		playerAnimator = GetComponent<Animator>();
+		if (!playerAnimator)
+		{
+			Debug.LogError("PlayerAnimatorManager is missing animator component", this);
+		}
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if (!playerAnimator)
+		{
+			return;
+		}
+
+		// deal with Jumping
+		AnimatorStateInfo stateInfo = playerAnimator.GetCurrentAnimatorStateInfo(0);
+		
+		// only allow jumping if we are running.
+		if (stateInfo.IsName("Base Layer.Run")) {
+
+			// when using trigger parameter
+			if (Input.GetButtonDown("Fire2")) {
+
+				playerAnimator.SetTrigger("Jump");
+			}
+		}
+
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		if (verticalInput < 0)
+		{
+			verticalInput = 0;
+		}
+
+		playerAnimator.SetFloat("Speed", horizontalInput * horizontalInput + verticalInput * verticalInput);
+		playerAnimator.SetFloat("Direction", horizontalInput, directionDampTime, Time.deltaTime);		
 	}
 }
